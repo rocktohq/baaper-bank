126521f9a5310c626b1f9dedc33ac9d4beeb3b03
Saidul Mursalin <itzmonir@gmail.com>
2023-08-15 00:11:47 +0600

    Initial Update

---
+++ assets/js/login.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Login System
+ * 
+**/
+
+var loginInfo = {
+    email: "gorib@gmail.com",
+    password: "gorib12345"
+};
+
+
+// If the login button is clicked
+document.getElementById("btn-login").addEventListener("click", function () {
+    const email = document.getElementById("email");
+    const emailValue = email.value;
+    const pass = document.getElementById("password");
+    const passValue = pass.value;
+
+    if (emailValue === "") {
+        alert("Error: Email Field is Empty!");
+    } else if (passValue === "") {
+        alert("Error: Password Field is Empty!");
+    } else {
+        login(emailValue, passValue);
+    }
+});
+
+
+// Login Function
+function login(email, pass) {
+    if (email === loginInfo.email && pass === loginInfo.password) {
+        // alert("Ok")
+    } else {
+        alert("Error: Wrong Email or Password!");
+    }
+}
